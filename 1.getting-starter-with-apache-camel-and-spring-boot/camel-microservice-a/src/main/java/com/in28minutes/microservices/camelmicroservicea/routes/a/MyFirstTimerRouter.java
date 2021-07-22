dac05108961a731f61b341875812b2f20703f194
gil-son <gilszon@gmail.com>
2021-07-22 19:54:36 -0300

    My First Timer Router

--- 1.getting-starter-with-apache-camel-and-spring-boot/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ 1.getting-starter-with-apache-camel-and-spring-boot/camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,2 +1,26 @@
-package com.in28minutes.microservices.camelmicroservicea.routes.a;public class MyFirstTimerRouter {
+package com.in28minutes.microservices.camelmicroservicea.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyFirstTimerRouter extends RouteBuilder {
+
+
+    @Override
+    public void configure() throws Exception {
+        // queue | timer
+        // transformation
+        // database | log
+        // Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
+        from("timer:first-timer") // queue | null
+                // .transform().constant("My constant Message")
+                .transform().constant("Time is now" + LocalDateTime.now())
+                .to("log:first-timer"); // database
+
+
+
+    }
 }
